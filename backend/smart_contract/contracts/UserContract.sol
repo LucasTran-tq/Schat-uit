22ffa8a575af89cb034a88c9bcd35f113489ccb5
LucasTran <lucastrantq@gmail.com>
2022-05-26 10:23:04 +0700

    save pubKey

--- backend/smart_contract/contracts/UserContract.sol
+++ backend/smart_contract/contracts/UserContract.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.6.0;
 
 contract UserContract {
-
     mapping(string => string) internal users;
 
     //
     string[] user_list;
 
     // add events
+    event UserEvent(string _userId, string _pubKey, uint256 index);
 
     // create
     function createUser(string memory _userId, string memory _pubKey) public {
@@ -15,32 +15,40 @@ contract UserContract {
 
         //
         user_list.push(_userId);
+
         // Event
+        emit UserEvent(_userId, _pubKey, uint256(user_list.length - 1));
     }
 
-    function getUser(string memory _userId) public view returns (string memory) {
+    function getUser(string memory _userId)
+        public
+        view
+        returns (string memory)
+    {
         return users[_userId];
     }
 
-    // // Function to retrieve 
+    // // Function to retrieve
     //  // values from the mapping
     //  function getUserList(
     //  ) view public returns (string[] memory) {
     //     return user_list;
     // }
 
-    function getUserByIndex(uint256 _index) public view returns(string memory) {
-        require(user_list.length >= _index, "Amount of user is smaller");
+    function getUserByIndex(uint256 _index)
+        public
+        view
+        returns (string memory)
+    {
+        require(user_list.length >= _index, 'Amount of user is smaller');
         return user_list[_index];
     }
-  
-    // Function to count number 
+
+    // Function to count number
     // of values in a mapping
-    function countUserInList(
-    ) view public returns (uint) {
+    function countUserInList() public view returns (uint256) {
         return user_list.length;
     }
-
 }
 
 // contract UserContract {
